625cad1d13bea5523d71d4ef527b92ede9d1cf2e
Adam Hess <adamhess1991@gmail.com>
2015-09-28 01:49:49 -0600

    debugging circle failure

--- spec/mock_class.rb
+++ spec/mock_class.rb
@@ -1,5 +1,7 @@
 class Foo
-  def self.bar(string); end
+  def self.bar(string)
+    puts string
+  end
 
   def subscribe_do_bar
     Sock::Client.new(redis: EM::Hiredis.connect).sub('hi', 'Foo', 'bar')
